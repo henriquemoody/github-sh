772b5098dbc56c0b69de176ece68071ec3360a7e
Ivo Nascimento <iannsp@gmail.com>
2012-07-26 14:06:48 -0300

    add the parser and the executer to workflow

--- prompt.php
+++ prompt.php
@@ -2,12 +2,23 @@
 $stdin = fopen('php://stdin','r');
 $out = fopen('php://stdout', 'w');
 require 'src/Executor.php';
+require 'src/Parser.php';
+require 'src/Github.php';
+$parser = new github\Parser;
 $exec   = new GitHub\Executor;
-$exec->doCmd("help");
+$github = new GitHub\GitHub;
+$exec->doCmd( $github->isValid($parser->parse("hello")));
 // the terminal CMD
 do{
 		$buf = readline("github> ");
 		readline_add_history($buf);
-		$cmd = $exec->doCmd($parser->parse($buf));
-		var_dump($buf);		
-} while($buf !="exit");
+		$cmd = $parser->parse($buf);
+		$github->isValid($cmd);
+		if ( $cmd->isValid) {
+			$cmd = $exec->doCmd($cmd);
+		} else {
+			echo "invalid gitHub Command.\n";
+		}
+
+} while(true);
+$exec->doCmd( $github->isValid($parser->parse("goodbye")));
