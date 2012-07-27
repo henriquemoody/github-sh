52bc9d32386d578cf85c9c68c70913f78383bf17
Ivo Nascimento <iannsp@gmail.com>
2012-07-26 22:06:14 -0300

    migrate code to bootstrap

--- github-sh.php
+++ github-sh.php
@@ -5,29 +5,7 @@
 error_reporting(E_ALL | E_STRICT);
 
 /* Timezone */
-date_default_timezone_set('UTC');
-
-/* Include path */
-set_include_path(implode(PATH_SEPARATOR, array(
-    __DIR__ . '/src',
-    get_include_path(),
-)));
-
-/* PEAR autoloader */
-spl_autoload_register(
-    function($className) {
-        $filename = strtr($className, '\\', DIRECTORY_SEPARATOR) . '.php';
-        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
-            $path .= DIRECTORY_SEPARATOR . $filename;
-            if (is_file($path)) {
-                require_once $path;
-                return true;
-            }
-        }
-        return false;
-    }
-);
-
+require 'bootstrap.php';
 $parser = new GitHub\Parser;
 $exec   = new GitHub\Executor;
 $github = new GitHub\GitHub;
