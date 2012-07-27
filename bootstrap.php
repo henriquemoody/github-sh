93b12b7ef8f436b888b46c208c23b0c5b6d9d0a1
Ivo Nascimento <iannsp@gmail.com>
2012-07-26 22:05:47 -0300

    migrate bootstrap instruction from git github-sh.php

---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set('UTC');
+set_include_path(implode(PATH_SEPARATOR, array(
+    __DIR__ . '/src',
+    get_include_path(),
+)));
+
+/* PEAR autoloader */
+spl_autoload_register(
+    function($className) {
+        $filename = strtr($className, '\\', DIRECTORY_SEPARATOR) . '.php';
+        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+            $path .= DIRECTORY_SEPARATOR . $filename;
+            if (is_file($path)) {
+                require_once $path;
+                return true;
+            }
+        }
+        return false;
+    }
+);
+
+
