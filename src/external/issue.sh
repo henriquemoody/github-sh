07038b72569d2b2fcb5522222facaa07923b8e1c
Henrique Moody <henriquemoody@gmail.com>
2012-07-27 00:49:42 -0300

    Create auth for issue command.

--- src/external/issue.sh
+++ src/external/issue.sh
@@ -1,38 +1,58 @@
-# github_external_issue NUMBER [ PROJECT [ USER ] ]
+# github_external_issue ACTION NUMBER [ PROJECT [ USER ] ]
 github_external_issue()
 {
     if [ -z "${1}" ]
     then
-        echo "You should define a issue number. (See \"help\" for more information)" 
+        github_internal_echo "You should define a issue action. (See \"help\" for more information)" 31 1>&2
         return 2
     fi
 
-    local number="${1}"
-    local project="${2}"
-    local user="${3}"
+    if [ -z "${2}" ]
+    then
+        github_internal_echo "You should define a issue number. (See \"help\" for more information)" 31 1>&2
+        return 3
+    fi
+
+    local action="${1}"
+    local number="${2}"
+    local project="${3}"
+    local user="${4}"
+    local extra_param=""
     local url
 
     if [ -z "${project}" ]
     then
-        if [ -z ${SCRIPT_VARS["project"]} ]
+        if [ -z "${SCRIPT_VAR_PROJ}" ]
         then
-            github_external_set project
+            read -e -p "Project name: " project
+        else
+            project="${SCRIPT_VAR_PROJ}"
         fi
-        project="${SCRIPT_VARS["project"]}"
     fi
 
     if [ -z "${user}" ]
     then
-        if [ -z ${SCRIPT_VARS["user"]} ]
+        if [ -z "${SCRIPT_VAR_USER}" ]
         then
-            github_external_set user
+            read -e -p "User or Organization: " project
+        else
+            user="${SCRIPT_VAR_USER}"
         fi
-        user="${SCRIPT_VARS["user"]}"
     fi
 
     url="https://api.github.com/repos/${user}/${project}/issues/${number}/comments"
-    echo $url
-    curl -L "${url}" | grep "body" | cut -d "\"" -f 4 | while read line 
+
+    if [ "${SCRIPT_VAR_USER}" != "" ]
+    then
+        extra_param="${SCRIPT_VAR_USER}"
+        if [ "${SCRIPT_VAR_PASS}" != "" ]
+        then
+            extra_param="${extra_param}:${SCRIPT_VAR_PASS}"
+        fi
+        extra_param="-u '${extra_param}'"
+    fi
+
+    curl "${extra_param}" -L "${url}" | grep "body" | cut -d "\"" -f 4 | while read line 
     do
         echo "${line}"
         echo
