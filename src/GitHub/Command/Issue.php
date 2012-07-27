e50b94670d0bdfe07655431a474273c212e2eb8d
Ivo Nascimento <iannsp@gmail.com>
2012-07-27 13:07:39 -0300

    First Implementation of request

--- src/GitHub/Command/Issue.php
+++ src/GitHub/Command/Issue.php
@@ -3,5 +3,21 @@ namespace GitHub\Command;
 
 class Issue extends Command
 {
+	
+	public function __get($name="default")
+	{
+		$p = $this->instruction->parameters;
+		if ($p[0] == (integer) $p[0]) {
+			return $this->getOne($p[0]);
+		}
 
+			var_dump($this->instruction);
+	}
+	public function getOne($id)
+	{
+			$result = $this->cApi->fetchUriData("https://api.github.com/repos/{$this->username}/{$this->project}/issues/{$id}");
+			$data =  json_decode($result);
+			var_dump($data);
+			return "aaa";
+	}
 }
