0603090fc7404f1679059819b3b9ff61f47fea45
Ivo Nascimento <iannsp@gmail.com>
2012-07-27 00:12:52 -0300

    a proto for fetch a Uri Data

--- src/GitHub/ClientApi.php
+++ src/GitHub/ClientApi.php
@@ -2,5 +2,20 @@
 namespace GitHub;
 class ClientApi
 {
-
+    public function FetchUriData($url)
+    {
+        $opts = array(
+            'http'=>array(
+            'method'=>"GET",
+            'header'=>"Accept-language: en\r\n" .
+            "Cookie: foo=bar\r\n"
+                )
+            );
+        $context = stream_context_create($opts);
+        $fp = fopen($url, 'r', false, $context);
+        $return = fgets($fp);
+        fclose($fp);
+        return $return;
+    }
+    
 }
