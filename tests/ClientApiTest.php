95df207d2841bba82636b4bd9c8874a8726cde89
Ivo Nascimento <iannsp@gmail.com>
2012-07-27 00:14:15 -0300

    use http://www.unixtimestamp.com/ to test if fetch happen

--- tests/ClientApiTest.php
+++ tests/ClientApiTest.php
@@ -6,5 +6,10 @@ class ClientApiTest extends PHPUnit_Framework_TestCase
     {
         $this->cApi = new \GitHub\ClientApi;
     }
+    public function testBasicGet()
+    {
+        $content = $this->cApi->FetchUriData("http://www.unixtimestamp.com/index.php");
+        $this->assertStringStartsWith('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">', $content);
+    }
 }
  
