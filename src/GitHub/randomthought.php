eabfe05736d178e93ac014c2e786e6276b86da19
Wesley Victhor Mendes Santiago <w.v.mendes.s@gmail.com>
2012-07-28 14:52:03 -0300

    Update src/GitHub/randomthought.php

--- src/GitHub/randomthought.php
+++ src/GitHub/randomthought.php
@@ -3,3 +3,6 @@ $thought[] = "Agua mole pedra dura, tanto bate ate que fura";
 $thought[] = "pau que nasce torto nunca se indireita";
 $thought[] = "script kid bom, eh script kid morto!!!";
 $thought[] = 'Nunca desafie a gravidade. Ela te derruba sempre.(ou não)';
+$thought[] = 'So long and thanks for all the fish.';
+$thought[] = 'I always thought something was fundamentally wrong with the universe';
+
