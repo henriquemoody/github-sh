c30caf9015bb0ac9d08efa90bcdc35eaf3072857
Domingos Teruel <mingomax@gmail.com>
2012-07-30 13:46:40 -0300

    Update src/GitHub/randomthought.php

--- src/GitHub/randomthought.php
+++ src/GitHub/randomthought.php
@@ -3,3 +3,5 @@ $thought[] = "Agua mole pedra dura, tanto bate ate que fura";
 $thought[] = "pau que nasce torto nunca se indireita";
 $thought[] = "script kid bom, eh script kid morto!!!";
 $thought[] = 'Nunca desafie a gravidade. Ela te derruba sempre.(ou não)';
+$thought[] = 'Mais vale um arquivo no HD do que dois baixando';
+$thought[] = 'Um jpeg vale mais que mil txt';
